60ebf2ed53a0e5f8d1d4c0ed6cf13f95aa53ee94
AdamGolecki <adam@golecki.pl>
2022-03-30 11:01:55 +0200

    example6

--- example6.cpp
+++ example6.cpp
@@ -1,13 +1,13 @@
 #include <cstdio>
 #include <memory>
 #include <string>
+#include <iostream>
 
 using ShoppingList = std::shared_ptr<std::FILE>;
 
 ShoppingList makeFile(const char* filename, const char* flags)
 {
-    /*Edit here*/
-    return std::shared_ptr<std::FILE>();
+    return std::shared_ptr<std::FILE>(fopen(filename, flags), [](std::FILE* shoppingList){fclose(shoppingList);});
 }
 
 class Partner
@@ -15,7 +15,7 @@ class Partner
 public:
     void addToFile(std::string element)
     {
-        /*Edit here*/
+        fprintf(shoppingList.get(), element.c_str());
     }
     ShoppingList shoppingList;
 };
